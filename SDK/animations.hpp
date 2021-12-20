d013a8b1d21ad79b0533f10f68621a7a1f31855d
Bartis1313 <bartis1313@gmail.com>
2021-12-20 11:33:09 +0100

    spaces

--- SDK/animations.hpp
+++ SDK/animations.hpp
@@ -6,9 +6,9 @@ public:
 	float m_animationTime;
 	float m_fadeOut;
 	void* m_studio;
-	int	m_dispatchedSrc;
-	int	m_dispatchedDst;
-	int	m_order;
+	int m_dispatchedSrc;
+	int m_dispatchedDst;
+	int m_order;
 	size_t m_sequence;
 	float m_prevCycle;
 	float m_weight;
@@ -16,5 +16,5 @@ public:
 	float m_playbackRate;
 	float m_cycle;
 	void* m_entity;
-	int	m_invalidatePhysicsBits;
+	int m_invalidatePhysicsBits;
 };
