c9bed54117f5f65525db3157cbc793624e119f40
Bartis1313 <bartis1313@gmail.com>
2023-01-02 20:31:08 +0100

    render view fix on many instances

--- cheats/hooks/renderView.cpp
+++ cheats/hooks/renderView.cpp
@@ -6,7 +6,7 @@
 
 hooks::renderView::value FASTCALL hooks::renderView::hooked(FAST_ARGS, const CViewSetup& view, const CViewSetup& hud, int clearFlags, int whatToDraw)
 {
-	original(thisptr, view, hud, clearFlags, whatToDraw);
-
 	RenderViewType::runAll(view);
+
+	original(thisptr, view, hud, clearFlags, whatToDraw);
 }
